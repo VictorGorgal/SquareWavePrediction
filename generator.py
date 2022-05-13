a01ab01fba2ce302fdb442129eb13c9d678c1476
Victor Gorgal <94933775+VictorGorgal@users.noreply.github.com>
2022-05-13 15:30:27 -0300

    Update generator.py

--- generator.py
+++ generator.py
@@ -1,6 +1,5 @@
 from scipy.signal import square
 from numpy import linspace, zeros, insert, pi
-import clean
 
 
 def generate(freq):
@@ -17,29 +16,8 @@ if __name__ == '__main__':
 
     x = linspace(0, 24, 48, endpoint=False)
     ar = generate(freq)
-
-    # Plot the square wave signal
     plot.plot(x, ar)
-
-    # Give a title for the square wave plot
     plot.title(f'spikes: {freq}')
-
-    # Give x axis label for the square wave plot
     plot.xlabel('Time (h)')
-
-    # Give y axis label for the square wave plot
     plot.ylabel('Amplitude')
-    plot.grid(True, which='both')
-
-    # Provide x axis and line color
-    plot.axhline(y=0, color='k')
-
-    # Set the max and min values for y axis
-    plot.ylim(0, 5)
-
-    # Display the square wave drawn
     plot.show()
-
-    # new = clean.clean_signal(24, 144, ar, threshold=0.1, debug=True)
-    # plot.plot(x, new)
-    # plot.show()
